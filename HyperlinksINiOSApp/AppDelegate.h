fd096baabe9ea52e20c23d97818e23a9bd364686
surajwebo <surajmirajkar@rediffmail.com>
2014-03-26 15:51:09 +0530

    Initial Commit

---
+++ HyperlinksINiOSApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HyperlinksINiOSApp
+//
+//  Created by Suraj on 26/03/14.
+//  Copyright (c) 2014 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HyperlinksINiOSAppTests/HyperlinksINiOSAppTests.m
@@ -0,0 +1,34 @@
+//
+//  HyperlinksINiOSAppTests.m
+//  HyperlinksINiOSAppTests
+//
+//  Created by Suraj on 26/03/14.
+//  Copyright (c) 2014 Suraj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HyperlinksINiOSAppTests : XCTestCase
+
+@end
+
+@implementation HyperlinksINiOSAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
