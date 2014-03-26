14dd9600c13163943a92966afdbce73b588587da
surajwebo <surajmirajkar@rediffmail.com>
2014-03-26 16:24:43 +0530

    HyperLink in iOS App using UITextView code completed.

--- HyperlinksINiOSApp/ViewController.m
+++ HyperlinksINiOSApp/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+#define X_COORDINATE 20
+#define Y_COORDINATE 140
+#define WIDTH  280
+#define HEIGHT 200
+
 @interface ViewController ()
 
 @end
@@ -18,6 +23,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *strText = [NSString stringWithFormat: @"Hey this is my blog: http://objectivecwithsuraj.blogspot.in/ which is hosted on https://www.blogger.com/. This is my blog here is share my experiences with all of you. So it can help others as well as me in future. "];
+    UITextView *txtViewClickableHyperLink = [[UITextView alloc] init];
+    [txtViewClickableHyperLink setFrame:CGRectMake(X_COORDINATE, Y_COORDINATE, WIDTH, HEIGHT)];
+    [txtViewClickableHyperLink setEditable:NO];
+    [txtViewClickableHyperLink setDataDetectorTypes:UIDataDetectorTypeLink];
+    [txtViewClickableHyperLink setText:strText];
+    [txtViewClickableHyperLink setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
+    [txtViewClickableHyperLink setFont:[UIFont systemFontOfSize:20.0f]];
+    [self.view addSubview:txtViewClickableHyperLink];
 }
 
 - (void)didReceiveMemoryWarning
